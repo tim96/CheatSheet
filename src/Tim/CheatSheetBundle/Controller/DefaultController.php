cbfee2931d17b0649599eb0fe34d32548b06b28c
Alexander Timofeev <tim96@yandex.ru>
2015-06-08 21:20:41 +0400

    Add new actions.

--- src/Tim/CheatSheetBundle/Controller/DefaultController.php
+++ src/Tim/CheatSheetBundle/Controller/DefaultController.php
@@ -9,10 +9,37 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="Home")
      * @Template()
      */
     public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/about", name="About")
+     * @Template()
+     */
+    public function aboutAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/symfony2", name="Symfony2")
+     * @Template()
+     */
+    public function symfony2Action()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/doctrine2", name="Doctrine2")
+     * @Template()
+     */
+    public function doctrine2Action()
     {
         return array();
     }
