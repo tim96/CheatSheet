b807077fa4ce04ff5f82e0ac4f0a9406039b3d77
Alexander Timofeev <tim96@yandex.ru>
2015-07-31 23:25:06 +0400

    Add feedback handler for future implementation.

---
+++ src/Tim/CheatSheetBundle/Handler/FeedbackHandler.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 31.07.2015
+ * Time: 23:21
+ */
+
+namespace Tim\CheatSheetBundle\Handler;
+
+class FeedbackHandler
+{
+    // todo extends from basehandler and add feedback logic
+}
