73fd85a05670bcb42910545a65d591929d85d33b
Alexander Timofeev <tim96@yandex.ru>
2015-08-01 22:32:40 +0400

    Add api controller. Add api routing.

--- src/Tim/CheatSheetBundle/Handler/BaseHandler.php
+++ src/Tim/CheatSheetBundle/Handler/BaseHandler.php
@@ -8,7 +8,53 @@
 
 namespace Tim\CheatSheetBundle\Handler;
 
-class BaseHandler
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+abstract class BaseHandler
 {
-    // todo add construct with service_container
+    /** @var  ContainerInterface */
+    protected $container;
+    /** @var  ObjectManager */
+    protected $om;
+    /** @var  string */
+    protected $entityClass;
+
+    protected $repository;
+    /** @var  UserInterface */
+    protected $user;
+
+    public function __construct(ContainerInterface $container, ObjectManager $om, $entityClass)
+    {
+        $this->container = $container;
+        $this->om = $om;
+        $this->entityClass = $entityClass;
+        $this->repository = $this->om->getRepository($this->entityClass);
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    public function getManager()
+    {
+        return $this->om;
+    }
+
+    public function getRepository()
+    {
+        return $this->repository;
+    }
+
+    public function setUser(UserInterface $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
