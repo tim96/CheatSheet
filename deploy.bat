81f9bb6a9ed74155ef512b4ab3f16b06c6c02a44
Alexander Timofeev <tim96@yandex.ru>
2015-08-01 20:22:10 +0400

    Change icon, Add deploy file for win system.

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+
+php app/console cache:clear
+php app/console cache:clear --env=prod --no-debug
+
+php app/console assets:install web --symlink
+
+php app/console assetic:dump
+php app/console assetic:dump --env=prod --no-debug
+
