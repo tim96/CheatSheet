85d2451a1d8e5ca43353f1e1239ea4d216617fd9
Alexander <tim96@yandex.ru>
2015-08-29 21:05:44 +0400

    Add sonata admin bundle, and configure sonata user bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,16 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new AntiMattr\GoogleBundle\GoogleBundle(),
 
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new \Sonata\BlockBundle\SonataBlockBundle(),
+            new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+
             new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new Tim\CheatSheetBundle\TimCheatSheetBundle(),
         );
 
