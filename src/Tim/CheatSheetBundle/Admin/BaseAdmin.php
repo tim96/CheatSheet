aace69fde8fa930710bac9b2a2de1365541c4fc7
Alexander <tim96@yandex.ru>
2015-08-31 17:18:31 +0400

    Add base admin layer for custom configuration.

---
+++ src/Tim/CheatSheetBundle/Admin/BaseAdmin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 31.08.2015
+ * Time: 20:01
+ */
+
+namespace Tim\CheatSheetBundle\Admin;
+
+use Monolog\Logger;
+use Sonata\AdminBundle\Admin\Admin;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Application\Sonata\UserBundle\Entity\User;
+
+abstract class BaseAdmin extends Admin
+{
+    /** @var \Monolog\Logger $logger */
+    private $logger;
+    /** @var ContainerInterface */
+    protected $container;
+
+    public function __construct($code, $class, $baseControllerName, $container = null)
+    {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->container = $container;
+        $this->logger = $this->container->get('logger');
+    }
+
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    protected function getUser()
+    {
+        /** @var User $user */
+        return $this->container->get('security.token_storage')->getToken()->getUser();
+    }
+}
