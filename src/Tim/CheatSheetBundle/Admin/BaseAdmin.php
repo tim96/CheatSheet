693dec02395a6fc5098a1a18201b183190f65bc3
Alexander <tim96@yandex.ru>
2015-09-05 17:00:44 +0400

    Add log when object create, update.

--- src/Tim/CheatSheetBundle/Admin/BaseAdmin.php
+++ src/Tim/CheatSheetBundle/Admin/BaseAdmin.php
@@ -85,11 +85,28 @@ abstract class BaseAdmin extends Admin
 
     public function postPersist($object)
     {
-
+        $user = $this->getUser();
+        if (method_exists($object, 'setUpdatedAt')) {
+            $object->setUpdatedAt(new \DateTime('now'));
+        }
+        if (method_exists($object, 'setCreatedAt')) {
+            $object->setCreatedAt(new \DateTime('now'));
+        }
+        if (method_exists($object, 'setAuthor')) {
+            $object->setAuthor($user);
+        }
+        $this->logger->addCritical("{$this->getUser()} #{$this->getUser()->getId()}. Create {$this->getClass()} #{$object->getId()}.");
     }
 
     public function postUpdate($object)
     {
-
+        $user = $this->getUser();
+        if (method_exists($object, 'setUpdatedAt')) {
+            $object->setUpdatedAt(new \DateTime('now'));
+        }
+        if (method_exists($object, 'setAuthor')) {
+            $object->setAuthor($user);
+        }
+        $this->logger->addCritical("{$this->getUser()} #{$this->getUser()->getId()}. Update {$this->getClass()} #{$object->getId()}.");
     }
 }
