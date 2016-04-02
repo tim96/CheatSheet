1a02b4c4e6445f0db38d2a0a9ef5c41dd9a86ec9
Alexander <tim96@yandex.ru>
2016-04-02 19:23:35 +0400

    Add admin screen for 'DoctrinePost'.

---
+++ src/Tim/CheatSheetBundle/Admin/DoctrinePostAdmin.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tim\CheatSheetBundle\Admin;
+
+// use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class DoctrinePostAdmin extends BaseAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('meta')
+            ->add('text')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            // ->add('isDeleted')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+            ->add('id')
+            ->add('meta')
+            ->add('text')
+            // ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            // ->add('isDeleted')
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            // ->add('id')
+            ->add('meta')
+            ->add('text')
+            ->add('description')
+            // ->add('createdAt')
+            // ->add('updatedAt')
+            // ->add('isDeleted')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('meta')
+            ->add('text')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('isDeleted')
+        ;
+    }
+}
---
+++ src/Tim/CheatSheetBundle/Controller/DoctrinePostAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tim\CheatSheetBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class DoctrinePostAdminController extends CRUDController
+{
+
+}
