e1da61654748b04894dc81e86dc1d0249a101ea3
Alexander <tim96@yandex.ru>
2016-04-29 11:38:02 +0400

    Small improvements in 'faq' page.

--- README.md
+++ README.md
@@ -11,11 +11,12 @@ TODO:
 - [X] Add route to show post using tags
 - [X] Try another pager bundle instead of knp (for example: Pagerfanta)
 - [X] Upgrade project from symfony 2.7 to symfony 2.8
+- [ ] Add separate pages for question like in posts
 - [ ] After upgrade project fix all deprecations in code
 - [ ] Add 'tinymce' inputs to edit forms
-- [ ] Add template to show popular posts
+- [X] Add template to show popular posts
 - [ ] Add some search engine to search content inside website
-- [ ] Add function to subscribe to new posts (Subscribe module)
+- [X] Add function to subscribe to new posts (Subscribe module)
 - [ ] Add block to dashboard
 
 A Symfony project created on June 7, 2015, 7:11 pm.
--- src/Tim/CheatSheetBundle/Resources/public/js/site.js
+++ src/Tim/CheatSheetBundle/Resources/public/js/site.js
@@ -2,8 +2,39 @@
 $(document).ready(function(){
     $(".spoilerButton").click(function (e) {
         e.preventDefault();
+
+        var elements = $(this).find('i');
+        for(var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            // if (hasClass(element, 'fa-arrow-left')) {
+            //     removeClass(element, 'fa-arrow-left');
+            //     addClass(element, 'fa-arrow-down');
+            // } else {
+            //     removeClass(element, 'fa-arrow-down');
+            //     addClass(element, 'fa-arrow-left');
+            // }
+            // console.log('Element', elements[i]);
+            if (hasClass(element, 'fa-arrow-left')) {
+                element.className = 'fa fa-arrow-down';
+            } else {
+                element.className = 'fa fa-arrow-left';
+            }
+        }
+
         var foo = $(this).attr('href');
-        $('#'+foo).slideToggle(250);
+        $('#'+foo).slideToggle(150);
     });
 });
 
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
