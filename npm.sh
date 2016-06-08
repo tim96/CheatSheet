a7fc5805da5b83b684af80e3a470044c75764c40
Alexander <tim96@yandex.ru>
2016-06-08 21:44:25 +0400

    Add solution to fix some problem with nodejs on linux.

--- npm.sh
+++ npm.sh
@@ -17,4 +17,8 @@ npm install gulp-concat
 npm install gulp-sourcemaps
 
 # to run all sripts from gulpfile.js
+
+# If you have some problem, like: error "/usr/bin/env: node: No such file or directory"
+# Add node to your path: ln -s /usr/bin/nodejs /usr/bin/node
+
 gulp
