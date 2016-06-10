1adf3c8aa1c269a1e0240f415d5d267d3e818776
Alexander <tim96@yandex.ru>
2016-06-10 23:08:43 +0400

    Add 'Application' entity.

---
+++ src/Tim/CheatSheetBundle/Entity/Application.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Tim\CheatSheetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Application
+ *
+ * @ORM\Table(name="application")
+ * @ORM\Entity(repositoryClass="Tim\CheatSheetBundle\Repository\ApplicationRepository")
+ */
+class Application
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     **/
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     **/
+    private $updatedAt;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_public", type="boolean", options={"default": false})
+     **/
+    private $isPublic;
+
+    // todo add dynamic connection between entities for save author id
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+        $this->description = null;
+        $this->isPublic = false;
+    }
+
+    public function __toString()
+    {
+        return $this->id ? $this->name : '';
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Application
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Application
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Application
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Application
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set isPublic
+     *
+     * @param bool $isPublic
+     *
+     * @return Application
+     */
+    public function setIsPublic($isPublic)
+    {
+        $this->isPublic = $isPublic;
+    
+        return $this;
+    }
+
+    /**
+     * Get isPublic
+     *
+     * @return bool
+     */
+    public function getIsPublic()
+    {
+        return $this->isPublic;
+    }
+}
