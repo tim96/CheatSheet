ba9386a7dd30add4091bb988c339e52d22fbbb1f
Alexander <tim96@yandex.ru>
2016-06-19 20:11:30 +0400

    Add example configuration for phpunit. Add help file for run phpunit commands.

---
+++ phpunit.sh
@@ -0,0 +1,12 @@
+
+# Xml file with phpunit configuration - app/phpunit.xml
+
+# Run phpunit to show all commands:
+# php phpunit.phar --help
+
+# Run phpunit to show version:
+# php phpunit.phar --version
+
+# Run phpunit to start test:
+# php phpunit.phar -c app
+
