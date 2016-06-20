6c33765053d893d901a7429ba579d04a1f5e7735
Alexander <tim96@yandex.ru>
2016-06-20 20:43:31 +0400

    Add example how to use 'beberlei/DoctrineExtensions'. See 'OrdersRepository' in TimExampleBundle.

--- src/Tim/ExampleBundle/Repository/OrdersRepository.php
+++ src/Tim/ExampleBundle/Repository/OrdersRepository.php
@@ -10,4 +10,27 @@ namespace Tim\ExampleBundle\Repository;
  */
 class OrdersRepository extends \Doctrine\ORM\EntityRepository
 {
+    /**
+     * @param array $ids
+     *
+     * @return \Doctrine\ORM\QueryBuilder
+     *
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function getByIds($ids = array())
+    {
+        $query = $this->createQueryBuilder('o');
+        $query
+            ->andWhere('o.id IN (:ids)')
+            ->setParameter('ids', array_values($ids))
+        ;
+
+        // Add example how to use beberlei DoctrineExtensions
+        // $emConfig = $this->getEntityManager()->getConfiguration();
+        // $emConfig->addCustomDatetimeFunction('timestampdiff', 'DoctrineExtensions\Query\Mysql\TimestampDiff');
+        //
+        // $query->addSelect('timestampdiff()');
+
+        return $query;
+    }
 }
