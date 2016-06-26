4d7b3f43278fb40d9baa464ad8dab1957c502932
Alexander <tim96@yandex.ru>
2016-06-26 14:28:23 +0400

    Add helper class to test script time execution and memory usage.

---
+++ src/Tim/UtilsBundle/Utils/Performance.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 6/26/2016
+ * Time: 1:19 PM
+ */
+
+namespace Tim\UtilsBundle\Utils;
+
+class Performance
+{
+    public static function start()
+    {
+        return microtime(true);
+    }
+
+    public static function result($timeStart)
+    {
+        return microtime(true) - $timeStart;
+    }
+
+    public static function getMemoryUsage()
+    {
+        // echo "Memory Usage: " . (memory_get_usage()/1048576) . " MB \n";
+        return memory_get_usage();
+    }
+
+    public static function getPeakMemoryUsage()
+    {
+        return memory_get_peak_usage();
+    }
+
+    public static function getMemoryLimit()
+    {
+        return ini_get('memory_limit');
+    }
+
+    public static function convertMemory($size)
+    {
+        $unit = array('b','kb','mb','gb','tb','pb');
+        return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+    }
+
+    public static function getMemoryUsageFormatted()
+    {
+        return self::convertMemory(memory_get_usage());
+    }
+
+    public static function getPeakMemoryUsageFormatted()
+    {
+        return self::convertMemory(memory_get_peak_usage());
+    }
+}
