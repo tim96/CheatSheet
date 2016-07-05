698bf372ec6ca3e5242572a45870787afeb8ef60
Alexander <tim96@yandex.ru>
2016-07-05 20:49:35 +0400

    Add class for listen auth handler: success/failure.

---
+++ src/Tim/ExampleBundle/Handler/AuthenticationHandler.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 7/5/2016
+ * Time: 8:39 PM
+ */
+
+namespace Tim\ExampleBundle\Handler;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class AuthenticationHandler implements AuthenticationFailureHandlerInterface, AuthenticationSuccessHandlerInterface
+{
+    /**
+     * This is called when an interactive authentication attempt fails. This is
+     * called by authentication listeners inheriting from
+     * AbstractAuthenticationListener.
+     *
+     * @param Request $request
+     * @param AuthenticationException $exception
+     *
+     * @return Response The response to return, never null
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // TODO: Implement onAuthenticationFailure() method.
+    }
+
+    /**
+     * This is called when an interactive authentication attempt succeeds. This
+     * is called by authentication listeners inheriting from
+     * AbstractAuthenticationListener.
+     *
+     * @param Request $request
+     * @param TokenInterface $token
+     *
+     * @return Response never null
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+}
