ef988cfcbf6d076d99d90401c46a017a49875487
Alexander <tim96@yandex.ru>
2016-07-14 22:17:54 +0400

    Add 'ListNode' file example.

---
+++ src/Tim/UtilsBundle/Algorithms/ListNode.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 7/14/2016
+ * Time: 10:15 PM
+ */
+
+class ListNode
+{
+    public $data;
+    public $next;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+        $this->next = NULL;
+    }
+
+    public function readNode()
+    {
+        return $this->data;
+    }
+}
