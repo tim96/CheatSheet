392696cbb2606d9b7c5034f6fad7e2b111880667
Alexander <tim96@yandex.ru>
2016-07-18 21:18:35 +0400

    Add draft test class 'DoubleLinkListTest' for 'DoubleLinkList'.

---
+++ src/Tim/UtilsBundle/Tests/DoubleLinkListTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tim\UtilsBundle\Tests;
+
+use Tim\UtilsBundle\Algorithms\DoubleLinkList;
+
+class DoubleLinkListTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDoubleLinkList()
+    {
+        $totalNodes = 100;
+        $theList = new DoubleLinkList();
+
+
+    }
+}
