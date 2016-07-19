b0f8cb7c1e691a1916a3f84b64f4e6ba3cefecae
Alexander <tim96@yandex.ru>
2016-07-19 20:55:15 +0400

    Add test example for 'SplDoublyLinkedList'.

--- src/Tim/UtilsBundle/Tests/DoubleLinkListTest.php
+++ src/Tim/UtilsBundle/Tests/DoubleLinkListTest.php
@@ -49,4 +49,51 @@ class DoubleLinkListTest extends \PHPUnit_Framework_TestCase
         $theList->deleteNode(10000);
         static::assertEquals($totalNodes - 3, $theList->totalNodes());
     }
+
+    /**
+     * For work with data structures in PHP exists extension SPL;
+     * http://php.net/manual/en/spl.datastructures.php
+     *
+     * A Doubly Linked List (DLL) is a list of nodes linked in both directions to each other.
+     * Iterator's operations, access to both ends, addition or removal of nodes have a cost of O(1) when the
+     * underlying structure is a DLL. It hence provides a decent implementation for stacks and queues.
+     */
+    public function testSplDoublyLinkedList()
+    {
+        $totalNodes = 100;
+        $theList = new \SplDoublyLinkedList();
+
+        for ($i = 1; $i <= $totalNodes; $i++) {
+            $theList->unshift($i);
+        }
+        static::assertEquals($totalNodes, $theList->count());
+
+        for ($i = 1; $i <= $totalNodes; $i++) {
+            $theList->push($i);
+        }
+        $totalNodes *= 2;
+        static::assertEquals($totalNodes, $theList->count());
+
+        $items = array();
+        $theList->setIteratorMode(\SplDoublyLinkedList::IT_MODE_FIFO);
+        for ($theList->rewind(); $theList->valid(); $theList->next()) {
+            $items[] = $theList->key();
+        }
+        static::assertCount($totalNodes, $items);
+
+        $theList->shift();
+        static::assertEquals($totalNodes - 1, $theList->count());
+
+        $theList->pop();
+        static::assertEquals($totalNodes - 2, $theList->count());
+
+        $theList->pop();
+        static::assertEquals($totalNodes - 3, $theList->count());
+
+        $theList->offsetUnset(25);
+        static::assertEquals($totalNodes - 4, $theList->count());
+
+        $this->setExpectedException(\OutOfRangeException::class);
+        $theList->offsetUnset(10000);
+    }
 }
