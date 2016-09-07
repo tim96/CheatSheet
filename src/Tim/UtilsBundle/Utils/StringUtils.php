73a6ce6a68a2c926217098e515d4e0a408d103e6
Alexander <tim96@yandex.ru>
2016-09-07 14:48:57 +0400

    Add example about double-quoted string escape sequences.

--- src/Tim/UtilsBundle/Utils/StringUtils.php
+++ src/Tim/UtilsBundle/Utils/StringUtils.php
@@ -10,6 +10,25 @@ namespace Tim\UtilsBundle\Utils;
 
 class StringUtils
 {
+    // Double-quoted string escape sequences:
+    //
+    // \n - Newline (ASCII 10)
+    // \r - Carriage return (ASCII 13)
+    // \t - Tab (ASCII 9)
+    // \v - Vertical Tab (ASCII 11)
+    // \e - escape (ASCII 27)
+    // \f - escape (ASCII 12)
+    // \\ - Backslash
+    // \$ - Dollar sign
+    // \" - Double quote
+    // \[0-7]{1,3} - the sequence of characters matching the regular expression is a character in octal notation, which
+    // silently overflows to fit in a byte (e.g. "\400" === "\000")
+    // \x[0-9A-Fa-f]{1,2} - the sequence of characters matching the regular expression is a character in
+    // hexadecimal notation
+    //
+    // \u{[0-9A-Fa-f]+} - the sequence of characters matching the regular expression is a Unicode codepoint, which
+    // will be output to the string as that codepoint's UTF-8 representation (added in PHP 7.0.0)
+
     // Finding a substring:
     public function findSubstring()
     {
