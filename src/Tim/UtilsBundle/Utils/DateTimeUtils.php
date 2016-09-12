6f6a724846330bab4ac3cd99acf61bf7cdb8a763
Alexander <tim96@yandex.ru>
2016-09-12 21:29:24 +0400

    Add example how to create date time object from string.

--- src/Tim/UtilsBundle/Utils/DateTimeUtils.php
+++ src/Tim/UtilsBundle/Utils/DateTimeUtils.php
@@ -25,4 +25,27 @@ class DateTimeUtils
         $result = "$now_2[2]:$now_2[1]:$now_2[0]"; // 21:25:32
         // printf('%s %d, %d',$a['month'],$a['mday'],$a['year']); // March 10, 2016
     }
+
+    protected function convertDateTime()
+    {
+        // Getting a specific epoch timestamp
+        // 7:45:03 PM on March 10, 1975, local time
+        // Assuming your "local time" is US Eastern time
+        $then = mktime(19, 45, 3, 3, 10, 1975);
+
+        // Getting a specific GMT-based epoch timestamp
+        // 7:45:03 PM on March 10, 1975, in GMT
+        $then = gmmktime(19, 45, 3, 3, 10, 1975);
+
+        // Getting a specific epoch timestamp from a formatted time string
+        // 7:45:03 PM on March 10, 1975, in a particular timezone
+        $then = \DateTime::createFromFormat(\DateTime::ATOM, "1975-03-10T19:45:03-04:00");
+
+        date_default_timezone_set('America/New_York');
+        // $stamp_future is 1733257500
+        $stamp_future = mktime(15,25,0,12,3,2024);
+
+        // $formatted is '2024-12-03T15:25:00-05:00'
+        $formatted = date('c', $stamp_future);
+    }
 }
